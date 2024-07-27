579200b8ae2d338cf64c328bb177c064f320123e
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-07-27 20:15:35 +0200

    frontend: fetch data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,25 @@
 import './App.css'
+import { useState, useEffect } from 'react'
 
 function App() {
+    const [data, setData] = useState(null)
 
-  return (
-    <>
-      <h1>Restaurants</h1>
-      <div className="card">
-        A project using Vite + React.
-      </div>
+    useEffect(() => {
+        async function getData() {
+            const response = await fetch('http://127.0.0.1:5000/main')
+            const responseData = await response.json()
+            setData(responseData)
+        }
+        getData()
+    }, [])
 
-    </>
-  )
+    return (
+        <>
+            <h1>Restaurants</h1>
+            <div className="card">A project using Vite + React.</div>
+            {data ? <div>{data.message}</div> : <div>Aun no hay datos</div>}
+        </>
+    )
 }
 
 export default App
