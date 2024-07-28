3e97dfab16caa7a698d6d14dc5212c19781acaa7
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-07-28 10:06:55 +0200

    frontend/backend: restaurants names are loaded, fetched and displayed

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,12 +1,17 @@
+import json
 from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route("/main")
 def main():
-    return {'message':"The server is working"}
+    with open("data/restaurants.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+    return data
+
 
 if __name__ == "__main__":
     # Executing will make the API available in
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,8 +16,21 @@ function App() {
     return (
         <>
             <h1>Restaurants</h1>
-            <div className="card">A project using Vite + React.</div>
-            {data ? <div>{data.message}</div> : <div>Aun no hay datos</div>}
+            {data ? (
+                <div>
+                    This is the current list of restaurants:
+                    <ul>
+                        {data.map((restaurant, index) => (
+                            <li key={index}>{restaurant.name}</li>
+                        ))}
+                    </ul>
+                </div>
+            ) : (
+                <div>There is still no data</div>
+            )}
+            <footer>
+                <div className="card">A project using Vite + React.</div>
+            </footer>
         </>
     )
 }
